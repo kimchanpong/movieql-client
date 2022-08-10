9a03ba773952a749be297e666cd0b68fd19f108d
KimChanHo <rlacksgh0826@gmail.com>
2022-08-10 15:25:01 +0900

    movie client 완료

--- src/client.js
+++ src/client.js
@@ -5,18 +5,4 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
-client.query({
-    query: gql`
-        {
-          allMovies {
-            id
-            title
-            rating
-            summary
-            genres
-          }
-        }
-    `
-}).then(data => console.log(data));
-
 export default client;
